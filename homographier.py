ae177b499ccb7456fd5bb51bdc5750f1ee5a9a09
RedBug312 <redbug312@gmail.com>
2018-06-08 23:59:45 +0800

    Refactor motion vector search

--- homographier.py
+++ homographier.py
@@ -13,10 +13,11 @@ class PanoFrame(pano.PanoImage):
 class Homographier:
 
     def __init__(self, root_frame):
-        self.pano_frames = list()
         root = PanoFrame(image=root_frame.img)
         root.H = np.identity(3)
-        self.pano_frames.append(root)
+        self.pano_frames = [root]
+        self.frame_shape = np.asarray(root.img.shape)
+        self.block_shape = np.asarray(self.frame_shape[:2]) // 16
 
     def find_homography_to_last(self, new_frame, mvs=None):
         ref_frame = self.pano_frames[-1]
@@ -24,45 +25,43 @@ class Homographier:
 
         Ix = np.gradient(cur_frame.img / 255, axis=1)
         Iy = np.gradient(cur_frame.img / 255, axis=0)
-        Ixx = np.sum(np.multiply(Ix, Ix), axis=2)
-        Ixy = np.sum(np.multiply(Ix, Iy), axis=2)
-        Iyy = np.sum(np.multiply(Iy, Iy), axis=2)
+        Ixx = cv2.GaussianBlur(np.sum(np.multiply(Ix, Ix), axis=2), (5, 5), 0)
+        Ixy = cv2.GaussianBlur(np.sum(np.multiply(Ix, Iy), axis=2), (5, 5), 0)
+        Iyy = cv2.GaussianBlur(np.sum(np.multiply(Iy, Iy), axis=2), (5, 5), 0)
 
         # beltrami = lambda st: 1 + np.linalg.det(st) + np.trace(st)
         beltrami = 1 + np.multiply(Ixx, Iyy) - np.multiply(Ixy, Ixy) + Ixx + Iyy
-        np.set_printoptions(threshold=np.nan)
 
         unique_coordinates = lambda cs: cs[np.unique(np.dot(cs, [[1], [1j]]), return_index=True)[1]]
-        # src_pts = np.multiply(np.transpose(np.where(beltrami > 1.005)), 1/16).astype('uint8')
-        src_pts = np.array(np.where(beltrami > 1.005), dtype=np.uint8).T // 16
-        src_pts = np.multiply(unique_coordinates(src_pts), 16) + 8
+        src_mbs = unique_coordinates(np.column_stack(np.where(beltrami > 1.005)) // 16)
+        at_margin = np.any(src_mbs == [0, 0], axis=1) + np.any(src_mbs == self.block_shape - 1, axis=1)
+        src_mbs = src_mbs[~at_margin]
 
         candidates = np.lib.stride_tricks.as_strided(
             ref_frame.img,
-            shape=(ref_frame.img.shape[0]-16, ref_frame.img.shape[1]-16, 16, 16, ref_frame.img.shape[2]),
+            shape=(self.frame_shape[0]-15, self.frame_shape[1]-15, 16, 16, self.frame_shape[2]),
             strides=ref_frame.img.strides[:2]+ref_frame.img.strides
         )
 
-        def find_dst(block):
-            # if mvs is not None:
-            #     mv = mvs[block[0]//16, block[1]//16]
-            #     if not np.isnan(mv[0]):
-            #         return mv + block
-            y, x = block.tolist()
-            h, w = cur_frame.img.shape[:2]
-            SAD = lambda candidate: np.sum(cv2.absdiff(cur_frame.img[y:y+16, x:x+16], candidate).astype(np.int))
-            ranges = candidates[max(0,y-8):min(y+9,h), max(0,x-8):min(x+9,w)]
-            errors = np.vectorize(SAD, signature='(16,16,3)->()')(ranges)
-            return np.array([max(0,y-8), max(0,x-8)]).astype(np.float) + \
-                   np.unravel_index(errors.argmin(), errors.shape)
+        def find_mv(pt):
+            SAD = lambda candidate: np.sum(cv2.absdiff(cur_frame.img[pt[0]:pt[0]+16, pt[1]:pt[1]+16], candidate))
+            mv = np.zeros(2, dtype=np.int)
+            # Three step search algorithm
+            for step in [4, 2, 1]:
+                # Python detects exceeded slicing end
+                ranges = candidates[max(0, pt[0]+mv[0]-step) : pt[0]+mv[0]+step+1 : step,
+                                    max(0, pt[1]+mv[1]-step) : pt[1]+mv[1]+step+1 : step]
+                errors = np.vectorize(SAD, signature='(16,16,3)->()')(ranges)
+                mv += step * np.subtract(np.unravel_index(errors.argmin(), errors.shape),
+                                         [pt[0] + mv[0] >= step, pt[1] + mv[1] >= step])
+            return mv
 
-        dst_pts = np.full(src_pts.shape, np.nan) if mvs is None \
-                  else mvs[src_pts.T[0] // 16, src_pts.T[1] // 16] + src_pts
-        known_dst_pts = ~np.isnan(dst_pts)[:, 0]
-        if not np.all(known_dst_pts):
-            dst_pts[~known_dst_pts] = np.vectorize(find_dst, signature='(2)->(2)')(src_pts[~known_dst_pts])
-        cur_frame.H = cv2.findHomography(dst_pts, src_pts, method=cv2.RANSAC)[0]
-        # print(cur_frame.H)
+        src_pts = src_mbs * 16 + 8
+        dst_mvs = np.full(src_mbs.shape, np.nan) if mvs is None \
+                  else mvs[src_mbs.T[0], src_mbs.T[1]]
+        known_dst_mvs = ~np.isnan(dst_mvs)[:, 0]
+        if not np.all(known_dst_mvs):
+            dst_mvs[~known_dst_mvs] = np.vectorize(find_mv, signature='(2)->(2)')(src_pts[~known_dst_mvs] - 8)
 
         self.pano_frames.append(cur_frame)
         return cur_frame
