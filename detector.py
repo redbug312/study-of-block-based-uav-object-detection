a56013dcbff09dda0475bc5a014117fca06c9dcd
RedBug312 <redbug312@gmail.com>
2018-06-10 14:03:14 +0800

    Store only 5 recent frames in detecting

--- detector.py
+++ detector.py
@@ -5,45 +5,37 @@ import cv2
 import homographier
 
 
-def detect(pano_frames, progress_bar=True):
-    detected_frames = list()
-    last_thresh = np.ones(pano_frames[0].img.shape[:2], dtype=np.uint8)
+def detect(frames, last_thresh=None):
+    last_thresh = np.ones(frames[2].img.shape[:2], dtype=np.uint8) \
+                  if last_thresh is None else last_thresh
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
 
-    adjacent_5frames = zip(pano_frames, pano_frames[1:], pano_frames[2:], pano_frames[3:], pano_frames[4:])
-    adjacent_5frames = tqdm(adjacent_5frames, total=len(pano_frames)-4) if progress_bar else adjacent_5frames
+    H2 = [np.dot(frames[1].H, frames[2].H),
+          frames[2].H,
+          np.identity(3),
+          np.linalg.inv(frames[3].H),
+          np.linalg.inv(np.dot(frames[3].H, frames[4].H))]
 
-    for index, frames in enumerate(adjacent_5frames):
-        H2 = [np.dot(frames[1].H, frames[2].H),
-              frames[2].H,
-              np.identity(3),
-              np.linalg.inv(frames[3].H),
-              np.linalg.inv(np.dot(frames[3].H, frames[4].H))]
+    image = np.array(frames[2].img)
+    dims = image.shape[:2][::-1]
 
-        image = np.array(frames[2].img)
-        dims = image.shape[:2][::-1]
+    img = [cv2.warpPerspective(f.img, H, dims) for f, H in zip(frames, H2)]
+    img = [cv2.cvtColor(i, cv2.COLOR_RGB2GRAY) for i in img]
+    img = [cv2.medianBlur(i, 5) for i in img]
 
-        img = [cv2.warpPerspective(f.img, H, dims) for f, H in zip(frames, H2)]
-        img = [cv2.cvtColor(i, cv2.COLOR_RGB2GRAY) for i in img]
-        img = [cv2.medianBlur(i, 5) for i in img]
+    D2 = [cv2.absdiff(i, img[2]) for i in img]
+    D2 = [cv2.threshold(D, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] for D in D2]
 
-        D2 = [cv2.absdiff(i, img[2]) for i in img]
-        D2 = [cv2.threshold(D, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] for D in D2]
+    thresh = (D2[0] * D2[4]) + (D2[1] * D2[3])
+    thresh = cv2.dilate(thresh, kernel, iterations=3)
+    thresh = cv2.erode(thresh, kernel, iterations=2)
 
-        thresh = (D2[0] * D2[4]) + (D2[1] * D2[3])
-        thresh = cv2.dilate(thresh, kernel, iterations=3)
-        thresh = cv2.erode(thresh, kernel, iterations=2)
+    cnts = cv2.findContours(thresh * last_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+    for cnt in cnts:
+        if cv2.contourArea(cnt) <= np.sum(kernel):
+            continue
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        if np.all(np.array([x, y, x+w, y+h]) - (0, 0, image.shape[1], image.shape[0])):
+            cv2.rectangle(image, (x, y), (x+w, y+h), (165, 66, 66), 2)
 
-        cnts = cv2.findContours(thresh * last_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
-        for cnt in cnts:
-            if cv2.contourArea(cnt) <= np.sum(kernel):
-                continue
-            (x, y, w, h) = cv2.boundingRect(cnt)
-            if np.all(np.array([x, y, x+w, y+h]) - (0, 0, image.shape[1], image.shape[0])):
-                cv2.rectangle(image, (x, y), (x+w, y+h), (165, 66, 66), 2)
-
-        # cv2.imwrite('detect/detect{:05d}.jpg'.format(index + 2), image)
-        detected_frames.append(homographier.PanoFrame(image=image))
-        last_thresh = thresh
-
-    return detected_frames
+    return homographier.PanoFrame(image=image), thresh
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess as sp
+import numpy as np
 from tqdm import tqdm
 from PIL import Image
 
@@ -12,20 +13,33 @@ frame_shape = (480, 640, 3)
 frame_count = 100
 frame_rate  = 30
 
-cmd = ['lib/FFmpeg-n4.0/ffmpeg',
-       '-flags2', '+export_mvs',
-       '-threads', '1',
-       '-nostats',
-       '-i', 'dataset/egtest01_short.mp4',
-       '-f', 'image2pipe',
-       '-pix_fmt', 'rgb24',
-       '-vcodec', 'rawvideo', '-']
+input_cmd = ['lib/FFmpeg-n4.0/ffmpeg',
+             '-flags2', '+export_mvs',
+             '-threads', '1',
+             '-nostats',
+             '-i', 'dataset/egtest01_short.mp4',
+             '-f', 'image2pipe',
+             '-pix_fmt', 'rgb24',
+             '-vcodec', 'rawvideo', '-']
 
-out_params = {'stdout': sp.PIPE, 'stderr': sp.DEVNULL}
-err_params = {'stdout': sp.DEVNULL, 'stderr': sp.PIPE}
+output_cmd = ['lib/FFmpeg-n4.0/ffmpeg',
+              '-y',
+              '-f', 'rawvideo',
+              '-vcodec', 'rawvideo',
+              '-s', '{0[1]}x{0[0]}'.format(frame_shape),
+              '-pix_fmt', 'rgb24',
+              '-r', str(frame_rate),
+              '-i', '-', '-an',
+              '-vcodec', 'libx264',
+              '-crf', '20',
+              'detect.mp4']
 
-with sp.Popen(cmd, **out_params) as out_proc, sp.Popen(cmd, **err_params) as err_proc,\
-     tqdm(total=frame_count) as pbar:
+out_params  = {'stdout': sp.PIPE, 'stderr': sp.DEVNULL}
+err_params  = {'stdout': sp.DEVNULL, 'stderr': sp.PIPE}
+save_params = {'stdout': sp.DEVNULL, 'stderr': sp.PIPE, 'stdin': sp.PIPE}
+
+with sp.Popen(input_cmd, **out_params) as out_proc, sp.Popen(input_cmd, **err_params) as err_proc,\
+     sp.Popen(output_cmd, **save_params) as save_proc, tqdm(total=frame_count) as pbar:
     # Unloop first iteration for root frame
     try:
         parser.digest_debug_info_before_mvs(err_proc.stderr)
@@ -36,6 +50,9 @@ with sp.Popen(cmd, **out_params) as out_proc, sp.Popen(cmd, **err_params) as err
     homo = homographier.Homographier(root_frame)
     pbar.update()
 
+    adjacent_5frames = [root_frame]
+    last_thresh = np.ones(root_frame.img.shape[:2], dtype=np.uint8)
+
     while True:
         image, mvs = parser.read_frame_with_mvs(out_proc.stdout, err_proc.stderr, frame_shape)
         if image is None:
@@ -43,35 +60,17 @@ with sp.Popen(cmd, **out_params) as out_proc, sp.Popen(cmd, **err_params) as err
 
         next_frame = homographier.PanoFrame(image=image)
         homo.find_homography_to_last(next_frame, mvs=mvs)
+
+        adjacent_5frames.append(next_frame)
+        if pbar.n >= 4:
+            detected_frame, last_thresh = detector.detect(adjacent_5frames, last_thresh)
+            adjacent_5frames = adjacent_5frames[1:]
+            try:
+                save_proc.stdin.write(detected_frame.img.tostring())
+            except IOError as err:
+                print(str(err) + '\n' + save_proc.stderr.read().decode('utf-8'))
+
         pbar.update()
-
-# root_frame = homographier.PanoFrame('dataset/EgTest01/frame00000.jpg')
-# homo = homographier.Homographier(root_frame)
-
-# for index in tqdm(range(1, 100)):
-#     next_frame = homographier.PanoFrame('dataset/EgTest01/frame{:05d}.jpg'.format(index))
-#     homo.find_homography_to_last(next_frame)
-
-cmd = ['lib/FFmpeg-n4.0/ffmpeg',
-       '-y',
-       '-f', 'rawvideo',
-       '-vcodec', 'rawvideo',
-       '-s', '{0[1]}x{0[0]}'.format(frame_shape),
-       '-pix_fmt', 'rgb24',
-       '-r', str(frame_rate),
-       '-i', '-', '-an',
-       '-vcodec', 'libx264',
-       '-crf', '20',
-       'detect.mp4']
-
-save_params = {'stdout': sp.DEVNULL, 'stderr': sp.PIPE, 'stdin': sp.PIPE}
-
-with sp.Popen(cmd, **save_params) as save_proc:
-    for frame in detector.detect(homo.pano_frames):
-        try:
-            save_proc.stdin.write(frame.img.tostring())
-        except IOError as err:
-            print(str(err) + '\n' + save_proc.stderr.read().decode('utf-8'))
 
 # panorama = stitcher.stitch(homo.pano_frames)
 # Image.fromarray(panorama).save('panorama.jpg')
